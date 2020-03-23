1cc015808807489b3ad751f23061fda796cd216f
sridhar <sridhar.subramani@thoughtworks.com>
2020-03-23 19:24:37 +0530

    Modify index API annotation

--- src/main/java/com/example/springdemo/cicd/springbootcicd/RestMainController.java
+++ src/main/java/com/example/springdemo/cicd/springbootcicd/RestMainController.java
@@ -1,12 +1,13 @@
 package com.example.springdemo.cicd.springbootcicd;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestMainController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         return "Welcome to Spring Boot CI-CD Excercise";
     }
